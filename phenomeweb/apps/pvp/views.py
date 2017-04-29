c48db095c9f89cd82adba207456fd033b7b1eb99
Maxat Kulmanov <coolmaksat@gmail.com>
2017-04-29 13:37:42 +0300

    res

--- phenomeweb/apps/pvp/views.py
+++ phenomeweb/apps/pvp/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import os
 
 from django.views.generic.edit import CreateView
 from django.views.generic import DetailView
@@ -30,6 +31,19 @@ class QueryResultView(DetailView):
     template_name = 'pvp/query/results.html'
 
     def get_context_data(self, **kwargs):
-        context = super(QueryCreateView, self).get_context_data(**kwargs)
+        context = super(QueryResultView, self).get_context_data(**kwargs)
         context['section'] = "pvp"
+        results = list()
+        res = self.get_object()
+        res_path = res.vcf_file.path + '.res'
+        if os.path.exists(res_path):
+            f = open(res_path, 'r')
+            lines = 0
+            for line in f:
+                lines += 1
+                if lines > 20:
+                    break
+                items = line.strip().split('\t')
+                results.append(items)
+        context['results'] = results
         return context
